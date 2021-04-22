3de7d2f588472d2dea15721f8e7c01b4ebbfbf04
ipikin <illia.pikin@dgngames.com>
2021-04-22 11:10:00 +0300

    Added fill method to WavFileHeader

--- include/WavFileHeader.h
+++ include/WavFileHeader.h
@@ -1,11 +1,14 @@
 
 /* 
- * File:   WavFileHeader.h
+ * File:   WavFilethis->h
  * Author: Ilya Pikin
  */
 
 #ifndef WAVFILEHEADER_H
 #define WAVFILEHEADER_H
+
+#include <cstring>
+#include <cstdio>
 
 #define WAV_FILE_HEADER_SIZE              44
 #define WAV_FILE_AUDIO_FORMAT_PCM         0x0001
@@ -25,6 +28,28 @@ struct WavFileHeader {
     uint16_t bitsPerSample;
     char subchunk2Id[4]; // "data"
     uint32_t subchunk2Size;
+
+    void fill(
+            size_t numSamplesPerChannel,
+            uint16_t bytesPerSample,
+            uint16_t numChannels,
+            uint32_t sampleRate,
+            uint16_t audioFormat) {
+
+        memcpy(this->chunkId, "RIFF", 4);
+        this->chunkSize = numSamplesPerChannel * numChannels * bytesPerSample + WAV_FILE_HEADER_SIZE - 8;
+        memcpy(this->format, "WAVE", 4);
+        memcpy(this->subchunk1Id, "fmt ", 4);
+        this->subchunk1Size = 16;
+        this->audioFormat = audioFormat;
+        this->numChannels = numChannels;
+        this->sampleRate = sampleRate;
+        this->byteRate = sampleRate * bytesPerSample * numChannels;
+        this->blockAlign = numChannels * bytesPerSample;
+        this->bitsPerSample = bytesPerSample * 8;
+        memcpy(this->subchunk2Id, "data", 4);
+        this->subchunk2Size = numSamplesPerChannel * numChannels * bytesPerSample;
+    }
 };
 
 #endif /* WAVFILEHEADER_H */
